ae9ebafc37c8a349a34733860fb3d73f60c0b309
Kenneth Manheim <Kenneth.Manheim@gmail.com>
2023-01-26 18:01:50 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/abschlussprufung/AbschlussprufungApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.abschlussprufung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlussprufungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbschlussprufungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/abschlussprufung/AbschlussprufungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.abschlussprufung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlussprufungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
